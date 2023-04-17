457428ea99fb77b5b5875d877183757c905af62c
Idad Wind <89760671+WillamSun@users.noreply.github.com>
2023-04-17 20:49:12 +0800

    Update

--- ScreenSaverForm.Designer.cs
+++ ScreenSaverForm.Designer.cs
@@ -28,21 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 0;
             // 
             // ScreenSaverForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ScreenSaverForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.ScreenSaverForm_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScreenSaverForm_UserActed);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ScreenSaverForm_UserActed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ScreenSaverForm.cs
+++ ScreenSaverForm.cs
@@ -1,27 +1,12 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Channels;
-using System.Text;
 using System.Windows.Forms;
 
 namespace ProgressBarSrc
 {
     public partial class ScreenSaverForm : Form
     {
-        private void SetText2PB(int value, ProgressBar pb)
-        {
-            if (pb.InvokeRequired)
-            {
-                SetValueCallBack svcb = new SetValueCallBack(SetText2PB);
-                Invoke(svcb, new object[] { value });
-            }
-            else pb.Value = value;
-        }
-
-        delegate void SetValueCallBack(int value, ProgressBar pb);
-
         private int maxProgressBarWidth = 300;
         private int minProgressBarWidth = 50;
         private int maxProgressBarHeight = 40;
@@ -32,20 +17,15 @@ namespace ProgressBarSrc
         private int maxProgressIncrement = 10;
         private int minProgressIncrement = 0;
 
-        private Random random = new Random();
+        readonly private Random random = new Random();
 
         public ScreenSaverForm()
         {
             InitializeComponent();
-
-            // 隐藏窗体边框
             FormBorderStyle = FormBorderStyle.None;
-
-            // 设置窗体全屏
             Bounds = Screen.PrimaryScreen.Bounds;
-
-            // 将窗体背景设置为黑色
             BackColor = Color.Black;
+            label1.ForeColor = Color.White;
         }
 
         private void ScreenSaverForm_Load(object sender, EventArgs e)
@@ -57,8 +37,8 @@ namespace ProgressBarSrc
                 Program.FilesINI iniFile = new Program.FilesINI(iniFilePath);
 
                 string background = iniFile.Read("Background", "Settings");
-                if (background.ToLower() == "light") BackColor = Color.White;
-                else if (background.ToLower() == "dark") BackColor = Color.Black;
+                if (background.ToLower() == "light") { BackColor = Color.White; label1.ForeColor = Color.Black; }
+                else if (background.ToLower() == "dark") { BackColor = Color.Black; label1.ForeColor = Color.White; }
                 else if (!string.IsNullOrEmpty(background)) MessageBox.Show(
                     "\"" + background + "\" is not a valid value for Background");
 
@@ -72,47 +52,44 @@ namespace ProgressBarSrc
                 minProgressIncrement = iniFile.ReadInteger("MinProgressIncrement", "Settings", maxProgressIncrement);
                 ProgressBarCount = iniFile.ReadInteger("ProgressBarCount", "Settings", ProgressBarCount);
             }
-            Cursor.Hide();
+            //Cursor.Hide();
             for (int i = 0; i < ProgressBarCount; i++)
             {
                 ProgressBar pb = new ProgressBar();
                 pb.Name = i.ToString();
-                pb.Tag = new int[] {
-                    random.Next(minMoveSpeed, maxMoveSpeed + 1), //Move speed
-                    random.Next(0,ProgressBarCount*100+1), //Delay time
-                    0, //Current passed time
-                };
-                Controls.Add(pb);
-                Timer timer = new Timer();
-                timer.Interval = 10;
-                Timer timer2 = new Timer();
-                timer2.Interval = 1000;
-                timer2.Tag = timer;
-                timer2.Tick += UpdateValue;
-                timer.Tag = new object[] { pb, timer2 };
-                timer.Tick += Update;
-                timer.Start();
-                timer2.Start();
+                pb.Tag = random.Next(minMoveSpeed, maxMoveSpeed + 1);
                 pb.Size = new Size(random.Next(minProgressBarWidth, maxProgressBarWidth + 1), random.Next(minProgressBarHeight, maxProgressBarHeight + 1));
                 pb.Location = new Point(-pb.Width, random.Next(0, Height - pb.Height + 1));
+                pb.MouseEnter += MouseHoverOnProgressBar;
+                pb.MouseLeave += MouseLeaveProgressBar;
+                Controls.Add(pb);
+                Timer timer = new Timer();
+                Timer timer2 = new Timer();
+                Timer timer3 = new Timer();
+                timer.Interval = 10;
+                timer2.Interval = 1000;
+                timer3.Interval = random.Next(0, ProgressBarCount * 100 + 1);
+                timer.Tick += Update;
+                timer2.Tick += UpdateValue;
+                timer3.Tick += MakeDelay;
+                timer2.Tag = timer;
+                timer3.Tag = new object[] { false, timer };
+                timer.Tag = new object[] { pb, timer2 };
+                timer3.Start();
             }
         }
 
         private void Update(object sender, EventArgs e)
         {
             ProgressBar pb = ((sender as Timer).Tag as object[])[0] as ProgressBar;
-            int[] tags = pb.Tag as int[];
-            if (tags[1] <= tags[2]) //Make delay
+            int tag = (int)pb.Tag;
+            pb.Location = new Point(pb.Location.X + tag, pb.Location.Y);
+            if (pb.Location.X > Width + pb.Width)
             {
-                pb.Location = new Point(pb.Location.X + tags[0], pb.Location.Y);
-                if (pb.Location.X > Width + pb.Width)
-                {
-                    pb.Size = new Size(random.Next(minProgressBarWidth, maxProgressBarWidth), random.Next(minProgressBarHeight, maxProgressBarHeight + 1));
-                    pb.Location = new Point(-pb.Width, random.Next(0, Height - pb.Height));
-                }
+                pb.Size = new Size(random.Next(minProgressBarWidth, maxProgressBarWidth), random.Next(minProgressBarHeight, maxProgressBarHeight + 1));
+                pb.Location = new Point(-pb.Width, random.Next(0, Height - pb.Height));
+                pb.Value = 0;
             }
-            else tags[2]++;
-            pb.Tag = tags;
         }
 
         private void UpdateValue(object sender, EventArgs e)
@@ -124,6 +101,42 @@ namespace ProgressBarSrc
                 while (pb.Value + ProgressIncrement > 100) ProgressIncrement--;
                 pb.Value += ProgressIncrement;
             }
+        }
+
+        private void MakeDelay(object sender, EventArgs e)
+        {
+            Timer main_timer = ((sender as Timer).Tag as object[])[1] as Timer;
+            object[] controls = main_timer.Tag as object[];
+            Timer this_sender = sender as Timer;
+            object[] this_sender_tag = this_sender.Tag as object[];
+            if ((bool)this_sender_tag[0])
+            {
+                main_timer.Start();
+                (controls[1] as Timer).Start();
+                this_sender.Stop();
+            }
+            else this_sender_tag[0] = true;
+            this_sender.Tag = this_sender_tag;
+        }
+
+        private void MouseHoverOnProgressBar(object sender, EventArgs e)
+        {
+            label1.Text = (sender as ProgressBar).Value.ToString() + "%";
+        }
+
+        private void MouseLeaveProgressBar(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+
+        private void ScreenSaverForm_UserActed(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+
+        private void ScreenSaverForm_UserActed(object sender, KeyPressEventArgs e)
+        {
+            Close();
         }
     }
 }
